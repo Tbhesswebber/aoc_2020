8ef98d5b7bdd1c8aa335c3cda978eeff22c09ef5
Tanner B. Hess Webber <tb@hesswebber.com>
2020-12-06 00:29:53 -0500

    day(6): solves challenges

--- lib/src/six/six.dart
+++ lib/src/six/six.dart
@@ -1,33 +1,69 @@
 import '../day.dart';
 
-class Six extends Day<List<OneArg>, List<TwoArg>> {
-
+class Six extends Day<List<Group>, List<Group>> {
   @override
-  List<OneArg> deserializeOneArgs(String args) {
-    // TODO: implement deserializeOneArgs
-    throw UnimplementedError();
-}
+  List<Group> deserializeOneArgs(String args) {
+    var groupResponses = args.split('\n\n');
+    return groupResponses.map((e) => Group(e)).toList();
+  }
 
   @override
   int one(args) {
-    // TODO: implement one
-    throw UnimplementedError();
+    return args.fold(0, (count, group) {
+      return count + group.uniqueAnswers.length;
+    });
   }
 
   @override
-  List<TwoArg> deserializeTwoArgs(String args) {
-    // TODO: implement deserializeTwoArgs
-    throw UnimplementedError();
-}
+  List<Group> deserializeTwoArgs(String args) {
+    var groupResponses = args.split('\n\n');
+    return groupResponses.map((e) => Group(e)).toList();
+  }
 
   @override
   int two(args) {
-    // TODO: implement one
-    throw UnimplementedError();
+    return args.fold(0, (count, group) {
+      return count + group.groupAnswers.length;
+    });
   }
-
 }
 
-class OneArg {}
+class Group {
+  final List<Person> members;
 
-class TwoArg {}
+  Group(String responses)
+      : members = splitOnBlankAndConvert(responses, to: (res) => Person(res));
+
+  int get size {
+    return members.length;
+  }
+
+  List<String> get groupAnswers {
+    var allAnswered = <String>[];
+
+    for (var answer in members[0].answers) {
+      if (members.every((element) {
+        return element.answers.contains(answer);
+      })) {
+        allAnswered.add(answer);
+      }
+    }
+
+    return allAnswered;
+  }
+
+  Set<String> get uniqueAnswers {
+    return members.fold(Set(), (answers, person) {
+      person.answers.forEach((element) {
+        answers.add(element);
+      });
+
+      return answers;
+    });
+  }
+}
+
+class Person {
+  final List<String> answers;
+  Person(String responses) : answers = responses.split('');
+}
